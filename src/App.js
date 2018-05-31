26934c86af8a887d3885db3dea2cdd6646b4a05b
LOTSYS\fdebon <ext.felix.debon@lotsys.com>
2018-05-31 14:07:18 +0200

    Try to get weather from api.openweathermap.org

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import fetch from 'isomorphic-fetch';
 import Carousel from './components/Carousel';
+import moment from 'moment-timezone'; 
 import './style.scss'
 
 /*injectGlobal`
@@ -31,24 +32,49 @@ class App extends Component {
 
   componentDidMount() {
     const self = this;
+    
     fetch('http://localhost:3001/cities')
       .then(function(resp) { return resp.json(); })
       .then(function(data) {
-        self.setState({ cities: data }) });
+        self.setState({ cities: data });
+        //console.log('data=',data);
+        /*data.forEach(function(city) {
+          console.log(city);
+          fetch('http://api.openweathermap.org/data/2.5/weather?q='+city.name+'&appid=5825570ef2944f8fc2586b00aa70843d')
+          .then(function(resp) { return resp.json(); })
+          .then(function(data) {
+            console.log('data',data);
+            let lon = data.coord.lon;
+            let lat = data.coord.lat;
+            let header = new Headers({
+              'Access-Control-Allow-Origin':'*',
+            
+            });
 
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=Bangkok&appid=5825570ef2944f8fc2586b00aa70843d')
-      .then(function(resp) { return resp.json(); })
-      .then(function(data) {
-        self.setState({ weather: data }) });        
+             fetch('https://api.timezonedb.com/v2/get-time-zone?key=OE8W0EWZJ0PL&format=json&by=position&lat='+lat+'&lng='+lon,{
+              headers: header
+            })
+             .then(function(res) { return res.json(); })
+              .then(function(data2) {
+                 console.log("timezonelab",data2);
+             });
+            
+            //let d = new Date(data.dt*1000);
+            //let name = city.name.replace(/ /g, "_");
+            //var c = moment.tz(data.dt*1000, "Asia/"+name).format('MMMM Do YYYY, h:mm:ss a');;
+            //console.log(c); 
+            //self.setState({ weather: data }) 
+          });   
+        });*/
+      });
 
+      //http://api.timezonedb.com/v2/get-time-zone?key=OE8W0EWZJ0PL&format=json&by=position&lat=13.75&lng=100.49
       //todo: give defaut objects if weather data is not available 
 
   }
 
   render() {
     const self = this;
-    console.log(this.state.weather);
-
     return (
       <div id="main-container">
         <div id="header">
